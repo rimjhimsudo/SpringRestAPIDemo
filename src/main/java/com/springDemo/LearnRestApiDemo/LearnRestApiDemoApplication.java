b042d6d1d97234352c3584a96507ef00ec42bd27
rimjhimsudo <rimjhimvaish@gmail.com>
2022-01-05 23:45:34 +0530

    ran successfull basic

---
+++ src/main/java/com/springDemo/LearnRestApiDemo/LearnRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springDemo.LearnRestApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRestApiDemoApplication.class, args);
+	}
+
+}
